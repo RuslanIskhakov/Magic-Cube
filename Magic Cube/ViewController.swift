7818b3b36adab0bac78c2631ae3370073f5382d9
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2017-11-09 09:53:08 +0500

    Empty project

---
+++ Magic Cube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic Cube
+//
+//  Created by Ruslan Iskhakov on 09.11.17.
+//  Copyright © 2017 Ruslan Iskhakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
